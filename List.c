adf2a0d5a665b89079045003582dd6a09636bb53
Ahlden Brough <ajabrough2@gmail.com>
2019-12-22 12:02:14 -0800

    removed comments + fixed memory error

--- List.c
+++ List.c
@@ -408,6 +408,7 @@ void deleteBack(List L){
   }
   else{
     L->back = L->back->prev;
+    L->back->next = NULL;
   }
   L->length--;
   freeNode(&N);
--- Matrix.c
+++ Matrix.c
@@ -61,13 +61,9 @@ Matrix newMatrix(int n){
 void freeMatrix(Matrix* pM){
   if(pM!=NULL && *pM!=NULL){
   	makeZero(*pM);
-    //printf("----------gets past make zero---------\n");
     for(int row = 0; row < size(*pM); row++){
-      //printf("pre if row = %d\n", row);
       if(length((*pM)->rows[row]) != 0){
-        //printf("before print list");
         freeList(&(*pM)->rows[row]);
-        //printf("frees list %d\n", row);
       }
     }
   	free((*pM)->rows);
@@ -75,7 +71,6 @@ void freeMatrix(Matrix* pM){
     (*pM)->size = 0;
     free(*pM);
   	*pM = NULL;
-    //printf("----------completed---------\n");
   }
 }
 
@@ -109,31 +104,24 @@ int equals(Matrix A, Matrix B){
     exit(EXIT_FAILURE);
   }
   if(A == B){
-     //printf("edge 1\n");
      return 1;
   }
   if(A->NNZ != B->NNZ){
-    //printf("edge 2\n");
     return 0;
   }
   if(size(A) != size(B)){
-    //printf("edge 3\n");
     return 0;
   }
   for(int z = 0; z < size(A); z++){
-    //printf("z = %d\n", z);
     if(length(A->rows[z]) == length(B->rows[z]) && (length(A->rows[z]) != 0) && (length(B->rows[z]) != 0) ){
-      //printf("length was the same\n");
       moveFront(A->rows[z]);
       moveFront(B->rows[z]);
       while(index(A->rows[z]) > -1 && index(B->rows[z]) > -1 ){  
         Entry a = get(A->rows[z]);
         Entry b = get(B->rows[z]);
         if(a->column != b->column && a->value != b->value){
-          //printf("index %0.1f !=  index %0.1f\n", (a->value), (b->value));
           return 0;
         }
-        //printf("moves next\n");
         moveNext(A->rows[z]);
         moveNext(B->rows[z]);
       }
@@ -151,12 +139,10 @@ int equals(Matrix A, Matrix B){
 // Re-sets M to the zero Matrix.
 void makeZero(Matrix M){
   for(int row = 0; row < size(M); row++){
-    //printf("makeZero row = %d\n", row + 1);
     if(length(M->rows[row]) != 0){
       while(index(M->rows[row]) != -1){
         Entry E = (Entry)get(M->rows[row]);
         changeEntry(M, row, E->column, 0);
-        //printf("deletes (%d, %d)\n", row + 1, col + 1);
 
       }
       clear(M->rows[row]);
@@ -186,7 +172,6 @@ void changeEntry(Matrix M, int i, int j, double x){
   //case 2: Mij != 0 and x == 0 inserting 0 and entry exists (delete entry)
   //case 3: Mij == 0 and x != 0 inserting non zero and entry does not exist (new entry)
   //case 4: Mij != 0 and x != 0 inserting non zero and entry exists (modify entry)
-  //printf("starts changeEntry\n");
   Entry E = newEntry(j, x);
   int Mij = 0;
   if(length(M->rows[i-1]) == 0 && x != 0){
@@ -194,7 +179,6 @@ void changeEntry(Matrix M, int i, int j, double x){
     M->NNZ++;
     return;
   }
-  //finding if entry exists and if so where
   if(length(M->rows[i-1]) > 0){
     moveFront(M->rows[i-1]);
   }
@@ -208,7 +192,6 @@ void changeEntry(Matrix M, int i, int j, double x){
   }
   //case 3
   if(Mij == 0 && x != 0){
-    //printf("case 3\n");
     moveFront(M->rows[i-1]);
     Entry current = (Entry)get(M->rows[i-1]);
     while(1){
@@ -231,17 +214,13 @@ void changeEntry(Matrix M, int i, int j, double x){
   }
   //case 2
   else if(Mij != 0 && x == 0){
-    //printf("case 2\n");
     Entry current = (Entry)get(M->rows[i-1]);
-    //freeEntry(&current);
     delete(M->rows[i-1]);
-    //current = NULL;
     M->NNZ--;
     return;
   }
   // case 4
   else if(Mij != 0 && x != 0){
-    //printf("case 4\n");
     Entry current = (Entry)get(M->rows[i-1]);
     current->value = x;
     return;
@@ -253,7 +232,6 @@ void changeEntry(Matrix M, int i, int j, double x){
 // copy()
 // Returns a reference to a new Matrix object having the same entries as A.
 Matrix copy(Matrix A){ // modify this please 
-  //printf("starts copy\n");
   Matrix N = newMatrix(size(A));
   for(int q = 0; q < size(A); q++){
     if(length(A->rows[q]) != 0){
@@ -276,7 +254,6 @@ Matrix copy(Matrix A){ // modify this please
 // transpose()
 // Returns a reference to a new Matrix object representing the transpose of A.
 Matrix transpose(Matrix A){
-  //printf("starts transpose\n");
   Matrix N = newMatrix(A->size);
   int old_column;
   for(int q = 0; q < size(A); q++){
@@ -300,7 +277,6 @@ Matrix transpose(Matrix A){
 // scalarMult()
 // Returns a reference to a new Matrix object representing xA.
 Matrix scalarMult(double x, Matrix A){
-  //printf("starts scalarMult\n");
   Matrix N = newMatrix(size(A));
   for(int q = 0; q < size(A); q++){
     if(length(A->rows[q]) > 0){
@@ -333,29 +309,19 @@ List helpRow(List P, List Q, int add_or_sub, Matrix M){
     moveFront(Q);
   }
   while(index(P) > -1 && index(Q) > -1){
-    //printf("first while loop\n");
     Entry current_P = (Entry)get(P);
     Entry current_Q = (Entry)get(Q);
-    //printf("(%d, %0.1f), (%d, %0.1f)\n", current_P->column, current_P->value, current_Q->column, current_Q->value);
     
     if(current_P->column == current_Q->column){
-      //printf("(%d, %0.1f), (%d, %0.1f)\n", current_P->column, current_P->value, current_Q->column, current_Q->value);
       if( (current_P->value + (add_or_sub * current_Q->value)) == 0){
-        //printf("condtion where adds to zero\n");
-        //printf("(%d, %0.1f) + %d * (%d, %0.1f)\n", current_P->column, current_P->value, add_or_sub, current_Q->column, current_Q->value);
         moveNext(P);
-        //printf("moves next P\n");
         if(P != Q){
           moveNext(Q);
-          //printf("moves next Q\n");
         }
-        //printf("~~~~~~~~~~~~~~~~~~if statement over~~~~~~~~~~~~~~~~~~\n");
         continue;
       }
       append(N, newEntry(current_P->column, current_P->value + (add_or_sub * current_Q->value)));
       M->NNZ++;
-      //printf("(%d, %0.1f) + %d * (%d, %0.1f)\n", current_P->column, current_P->value, add_or_sub, current_Q->column, current_Q->value);
-      //printf("appends (%d, %0.1f)\n", current_P->column, current_P->value + (add_or_sub * current_Q->value));
       moveNext(P);
       moveNext(Q);
     }
@@ -363,38 +329,27 @@ List helpRow(List P, List Q, int add_or_sub, Matrix M){
     else if(current_P->column < current_Q->column){
       append(N, newEntry(current_P->column, current_P->value));
       M->NNZ++;
-      //printf("(%d, %0.1f) + %d * (%d, %0.1f)\n", current_P->column, current_P->value, add_or_sub, current_Q->column, current_Q->value);
-      //printf("appends (%d, %0.1f)\n", current_P->column, current_P->value);
       moveNext(P);
     }
     else if(current_P->column > current_Q->column){
       append(N, newEntry(current_Q->column, current_Q->value * add_or_sub));
       M->NNZ++;
-      //printf("(%d, %0.1f) + %d * (%d, %0.1f)\n", current_P->column, current_P->value, add_or_sub, current_Q->column, current_Q->value);
-      //printf("appends (%d, %0.1f)\n", current_Q->column, current_Q->value * add_or_sub);
       moveNext(Q);
     }
-    //printf("-------------------------------------\n");
   }
   if(index(P) == -1 && index(Q) != -1){
     while(index(Q) > -1){
-      //printf("second while loop\n");
       Entry current_Q = (Entry)get(Q);
       append(N, newEntry(current_Q->column, current_Q->value * add_or_sub));
       M->NNZ++;
-      //printf("(%d, %0.1f)\n", current_Q->column, current_Q->value);
-      //printf("appends (%d, %0.1f)\n", current_Q->column, current_Q->value * add_or_sub);
       moveNext(Q);
     }
   }
   else if(index(Q) == -1 && index(P) != -1){
     while(index(P) > -1){
-      //printf("third while loop\n");
       Entry current_P = (Entry)get(P);
       append(N, newEntry(current_P->column, current_P->value));
       M->NNZ++;
-      //printf("(%d, %0.1f)\n", current_P->column, current_P->value);
-      //printf("appends (%d, %0.1f)\n", current_P->column, current_P->value);
       moveNext(P);
     }
   }
@@ -429,9 +384,6 @@ Matrix diff(Matrix A, Matrix B){
   }
   Matrix N = newMatrix(size(A));
   for(int row = 0; row < size(A); row++){
-    //printf("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n");
-    //printf("row %d\n", row + 1);
-    //printf("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n");
     N->rows[row] = helpRow(A->rows[row], B->rows[row], -1, N);
   }
   return N;
@@ -439,7 +391,6 @@ Matrix diff(Matrix A, Matrix B){
 
 
 double vectorDot(List P, List Q){
-  //printf("starts vectorDot\n");
   if(length(P) > 0){
     moveFront(P);
   }
@@ -448,25 +399,20 @@ double vectorDot(List P, List Q){
   }
   double sum = 0;
   while(index(P) > -1 && index(Q) > -1){
-    //printf("enters while\n");
     Entry current_P = (Entry)get(P);
     Entry current_Q = (Entry)get(Q);
     if(current_P->column == current_Q->column){
-      //printf("first if\n");
       sum = sum + (current_P->value * current_Q->value);
       moveNext(P);
       moveNext(Q);
     }
     else if(current_P->column < current_Q->column){
-      //printf("second if\n");
       moveNext(P);
     }
     else if(current_P->column > current_Q->column){
-      //printf("third if\n");
       moveNext(Q);
     }
   }
-  //printf("exits vectorDot\n");
   return sum;
 }
 
@@ -500,10 +446,8 @@ Matrix product(Matrix A, Matrix B){
 // list of pairs "(col, val)" giving the column numbers and non-zero values 
 // in that row. The double val will be rounded to 1 decimal point.
 void printMatrix(FILE* out, Matrix M){
-  //printf("starts printMatrix\n");
   for(int p = 0; p < size(M); p++){
   	if(length(M->rows[p]) == 0){
-  	  //printf("length of row was 0\n");
       continue;
   	}
   	moveFront(M->rows[p]);
